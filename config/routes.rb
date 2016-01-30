359797a63a036f647301c6592b2659535bfff038
West Kraemer <WestKraemer@gmail.com>
2016-01-30 13:06:53 -0500

    add viewport scale, charset, and dynamic titles to pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
+  # get 'static_pages/home'
+  root 'static_pages#home'
+  
   get 'static_pages/about'
 
   get 'static_pages/webdev'
@@ -13,7 +14,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
