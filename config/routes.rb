408fdf69d6af2a2677807d96463f504d1954624c
West Kraemer <WestKraemer@gmail.com>
2016-01-30 17:15:18 -0500

    add background image; begin sizing, need breakpoints.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   #root route
   root 'static_pages#home'
+
   #page routes for portfolio content
-  get 'about' => 'static_pages#about'
-  get 'webdev' => 'static_pages#webdev'
+  get 'about'   => 'static_pages#about'
+  get 'webdev'  => 'static_pages#webdev'
   get 'writing' => 'static_pages#writing'
   get 'contact' => 'static_pages#contact'
 
